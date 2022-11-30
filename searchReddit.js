ae21cec1469838644bed5e78517b866cd2cd95a5
izzy1090 <michaelisabella1@gmail.com>
2022-11-30 15:27:29 -0500

    Implemented filter function and cleaned up scripts
    I also cleaned up the scripts to be clearer re: their intended
    purpose as well as the overall control flow of the build.
    
    Definite future additions:
    *HTML / CSS (i.e. UI) implementation
    *Ideally I would like to allow a user to interact with the app easily
    
    Possible future additions:
    *Individual user authentication
    *Script that beautifies txt files living in the file directory
    *Combine past searches into one .txt file
    *Schedule searches

--- searchReddit.js
+++ searchReddit.js
@@ -1,94 +1,26 @@
-// dependency required for encoding API tokens and keys
-const config = require('dotenv').config()
-
-// to use fetch with earlier versions of node
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-// dependencies needed to run the script
-const fs = require('fs')
-
 // JS files needed to run this script
-const run = require('./userInput')
+const token = require('./authenticateAPI')
+const filter = require('./filterData')
+const user = require('./userInput')
+const readFile = require('./readFile')
 
-// initialize variable to current date 
-const today = new Date().toDateString().split(' ').join('_').toLowerCase();
-// initialize variable to exact time by mins.
-const exactMinute = new Date().getMinutes();
-// use previous variable to build unique filename
-const uniqueFilename = exactMinute + '_' + today + run.searchTerm.split(' ').join('_')
-
-// declared variables to use for OAuth with Reddit's API
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
-const api_key = process.env.API_KEY;
-const api_secret = process.env.API_SECRET;
-// declared bearer_token variable to initialize 
-    // with bearer token returned from OAuth fetch POST below
-var bearer_token;
-
-// when invoked function generates access token to use for authentication 
-    // while utilizing Reddit's API
-// input: nothing
-// output: initializes bearer_token with generated access token
-const getAccessToken = async () => {
+// order of function invocations to successfully compile current build
+const runScript = async () => {
     try {
-        // first make a post request to reddit's API for an access_token
-        return await fetch('https://www.reddit.com/api/v1/access_token', {
-                method: 'POST',
-                headers: {
-                    'User-Agent': 'User agent',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    // use reddit app's api_key and api_secret for authorization
-                        // btoa is a method that creates a Base64-encoded ASCII string - basically converts binary to string
-                    'Authorization': 'Basic ' + Buffer.from(`${api_key}:${api_secret}`).toString('base64') },
-                // use the associated account's username and password
-                body: `grant_type=password&username=${username}&password=${password}`})
-        // then return the bearerToken as a JSON
-        .then(getBearerToken=>{
-            return getBearerToken.json() })
-        .then(returnedToken=> {
-            // then initialize returnedJSON to values of an empty object
-            const bearerTokenCache = Object.values(returnedToken)
-            // return earlier declared bearer_token 
-            // initialized to corresponding value of the first key in cache object 
-            return bearer_token = bearerTokenCache[0] }) }
-    catch (err){
+        // generate access token for authentication with reddit's API
+        return await token.getAccessToken()
+        // authenticate and perform search using user generated search term and save a file locally
+        .then(() => { return token.oauthSearch(`${token.uniqueFilename}`) })
+        // read in saved file from search and assign it to a variable
+        .then(() => { return readFile.asyncReadFile(`${token.uniqueFilename}.txt`) })
+        // remove rawData from it's parent object and initialize it to a new object
+        .then(rawData => { return filter.isoChildObj(rawData) })
+        // then take returned object and initialize it to evaluted
+            // results of invoking filterData with object as a passed-in arg
+        .then(object => { return filter.filterData(object) })
+    } catch (err) {
         console.log(err)
     }
 }
-
-// when invoked, function authenticates w/Reddit's API and 
-    // creates a file as well as returns search results based on user-generated parameters
-// input: searchTerm and filename
-// output: raw json data and save a file locally
-const oauthSearch = async (filename) => {
-    try {
-        // promise to fetch a query from reddit's API using user-generated search terms
-            // and pull all matching threads
-        return await fetch(`https://oauth.reddit.com/r/all/search/?q=${run.searchTerm}&limit=${run.threadLimit}&sort=${run.sortFilter}`, 
-        { headers: {
-            // authorize with previously generated bearer_token here
-            Authorization: `bearer ${bearer_token}`}
-        })
-        // then convert the response of fetch request into a json
-        .then( response => response.json() )
-        // convert returned json into a string and write it a saved file in the directory
-            // may not need this, but it's good having raw data saved locally
-        .then( data => {
-            const textDoc = JSON.stringify(data)
-            fs.writeFile(`${filename}.txt`, textDoc, function(err){
-                if(err){
-                    console.log(err) }
-            })
-            return {textDoc} 
-        }) 
-        } catch (err){
-            console.log(err)
-    }
-}
-
-module.exports = {
-    getAccessToken, 
-    oauthSearch,
-    uniqueFilename
-}
+// invoke runScript function and log the results to the console
+runScript().then(data=>console.log(data))
--- userInput.js
+++ userInput.js
@@ -1,3 +1,6 @@
+const { existsSync } = require("fs");
+const { exit } = require("process");
+
 // declare a variable to export out to other scripts
 let searchTerm = "";
 let sortFilter = "hot";
@@ -5,8 +8,7 @@ let threadLimit = 0;
 
 // need to write userInput function accepts user search term
 // input: search term, thread limit, and sort category
-// output: initializes global variables with passed-in args
-// declare a function called userInput
+// output: initializes global variables with passed-in args 
 function userInput(search, limit, sort) {
     try {
         // if search and threadLimit are found and sort is missing
@@ -15,14 +17,15 @@ function userInput(search, limit, sort) {
             searchTerm = search
             threadLimit = limit
             console.log("Hot results are shown by default. Thread count caps at 100.") } 
-        // if search, threadLimit and sort are found 
+        // if search, threadLimit and sort are all found 
         else if (search && threadLimit > 25 || threadLimit < 100 && sort.match(/['relevance','hot','new','comments', 'old']/)) {
             // initialize all variables to passed-in args
             console.log(`Sorting threads by ${sort}... Thread count caps at 100.`)
             searchTerm = search
             threadLimit = limit
             sortFilter = sort } 
-        else if (threadLimit > 100 || !sort.match(['relevance','hot','new','comments', 'old'])){
+        // if thread count is greater than 100 or sort doesn't match log error message
+        else if (threadLimit > 100 || !sort.match(['relevance','hot','new','comments', 'old'])) {
             console.log("Bad search request, your results won't be accurate.") }
     } 
     // else return an error
@@ -33,7 +36,7 @@ function userInput(search, limit, sort) {
 
 // for sort pick one of (relevance, hot, top, new, comments, old)
     // at this time date ranges are not possible via Reddit's API
-userInput('elden ring', 50, 'old')
+userInput('crypto', 50, 'hot')
 
 // this can be the script I use to run the order of actions for everything
 
