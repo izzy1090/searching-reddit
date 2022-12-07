f5762a4c833a71c7dadc761d4b5a18ef561d3b77
izzy1090 <michaelisabella1@gmail.com>
2022-12-06 21:15:12 -0500

    Finished HTML layout and CSS styling
    Started incorporating React to change states and allow a user
    add enter search into a search bar.
    
    Need to do more research into state changes, react and how to
    time a state change after the user clicks the search button.

--- JS/userInput.js
+++ JS/userInput.js
@@ -1,44 +1,43 @@
-// declare a variable to export out to other scripts
-let searchTerm = "";
-let sortFilter = "hot";
-let threadLimit = 0;
+// // declare a variable to export out to other scripts
+// let searchTerm = "";
+// let sortFilter = "hot";
+// let threadLimit = 0;
 
-// need to write userInput function accepts user search term
-// input: search term, thread limit, and sort category
-// output: initializes global variables with passed-in args 
-function userInput(search, limit, sort) {
-    try {
-        // if search and threadLimit are found and sort is missing
-        if (search && threadLimit > 25 || threadLimit < 100 && !sort) {
-            // initialize global variables to match user input
-            searchTerm = search
-            threadLimit = limit
-            console.log("Hot results are shown by default. Thread count caps at 100.") } 
-        // if search, threadLimit and sort are all found 
-        else if (search && threadLimit > 25 || threadLimit < 100 && sort.match(/['relevance','hot','new','comments', 'old']/)) {
-            // initialize all variables to passed-in args
-            console.log(`Sorting threads by ${sort}... Thread count caps at 100.`)
-            searchTerm = search
-            threadLimit = limit
-            sortFilter = sort } 
-        // if thread count is greater than 100 or sort doesn't match log error message
-        else if (threadLimit > 100 || !sort.match(['relevance','hot','new','comments', 'old'])) {
-            console.log("Bad search request, your results won't be accurate.") }
-    } 
-    // else return an error
-    catch (err){
-        console.log(err)
-    }    
-}
+// // need to write userInput function accepts user search term
+// // input: search term, thread limit, and sort category
+// // output: initializes global variables with passed-in args 
+// function userInput(search, limit, sort) {
+//     try {
+//         // if search and threadLimit are found and sort is missing
+//         if (search && threadLimit > 25 || threadLimit < 100 && !sort) {
+//             // initialize global variables to match user input
+//             searchTerm = search
+//             threadLimit = limit
+//             console.log("Hot results are shown by default. Thread count caps at 100.") } 
+//         // if search, threadLimit and sort are all found 
+//         else if (search && threadLimit > 25 || threadLimit < 100 && sort.match(/['relevance','hot','new','comments', 'old']/)) {
+//             // initialize all variables to passed-in args
+//             console.log(`Sorting threads by ${sort}... Thread count caps at 100.`)
+//             searchTerm = search
+//             threadLimit = limit
+//             sortFilter = sort } 
+//         // if thread count is greater than 100 or sort doesn't match log error message
+//         else if (threadLimit > 100 || !sort.match(['relevance','hot','new','comments', 'old'])) {
+//             console.log("Bad search request, your results won't be accurate.") }
+//     } 
+//     // else return an error
+//     catch (err){
+//         console.log(err)
+//     }    
+// }
 
-// for sort pick one of (relevance, hot, top, new, comments, old)
-    // at this time date ranges are not possible via Reddit's API
-userInput('crypto', 50, 'new')
+// // // for sort pick one of (relevance, hot, top, new, comments, old)
+// //         // at this time date ranges are not possible via Reddit's API
+// userInput('', 100, 'hot')
 
-// this can be the script I use to run the order of actions for everything
-
-module.exports = {
-    searchTerm,
-    threadLimit, 
-    sortFilter
-}
+// module.exports = {
+//     searchTerm,
+//     threadLimit, 
+//     sortFilter,
+//     userInput
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,63 @@
-// currently: http://localhost:3001/
-
 // JS scripts needed to run this script
 const filter = require('../JS/filterData')
 const user = require('../JS/userInput')
+
+// imports necessary for HTML
+import Head from 'next/head';
+import Image from 'next/image';
+import redditLogo from '../public/images/reddit_logo.png'
+import searchButton from '../public/images/magnifying-glass.jpg'
+import { useState } from 'react';
+
+// declare a variable to export out to other scripts
+let searchTerm = "";
+let sortFilter = "";
+let threadLimit = 0;
+
  
-function page ( { results }) {
+function homePage ( { results }) {
+    const [searchInput, setSearchInput] = useState([]);
+
+    const searchItems = (searchTerm) => {
+        setSearchInput(searchTerm)
+    }
+    function genSearch() {
+        const s = searchInput
+        console.log(results)
+        userInput(s, 100, 'old')
+    }
+    
+    const posts = results.map(data=>{
+        return (
+            <div className="subreddits-container" key={data}>
+                <header id="threads-sizing">{data.subreddit}</header>
+                <div id="threads-sizing">{data.author_fullname}</div>
+                <div id="threads-sizing">{data.title}</div>
+                <div id="threads-sizing">{data.selftext}</div>
+                <div id="threads-sizing">Awards Received: {data.total_awards_received}</div>
+                <div id="threads-sizing">URL: {data.url}</div>
+            </div>
+        )
+    });    
     return (
-        <div>
+        <>
+        <Head>
+            <title>Home</title>
+            <meta charSet='utf-8'></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        </Head>
             <header>
-                <div class="search-container">
-                    <img src='/public/images/reddit_logo' alt='reddit logo' id="reddit-logo"></img>
+                <div className="search-container">
+                    <Image src={redditLogo} id="search-bar-icons" alt="Png of reddit logo"/>
                     <div id="header-title">subreddits & threads</div>
-                    <div id="search-bar">search reddit...</div>
+                    <input placeholder='search reddit...' onChange={ (e) => searchItems(e.target.value) } type='search' id='search-bar'></input>
+                    <Image src={searchButton} id="search-bar-icons" onClick={genSearch} alt='Graphic illustration of a magnifying glass'/>
+                </div>
+                <div className="page-contents">
+                    {posts}
                 </div>
             </header>
-            <div>
-                <div>
-                    {
-                        results.map(data=>{
-                            return (
-                                <div class="subreddits-container" key={data}>
-                                    <header id="threads-sizing">{data.subreddit}</header>
-                                    <div id="threads-sizing">{data.author_fullname}</div>
-                                    <div id="threads-sizing">{data.title}</div>
-                                    <div id="threads-sizing">{data.selftext}</div>
-                                    <div id="threads-sizing">Awards Received: {data.total_awards_received}</div>
-                                    <div id="threads-sizing">URL: {data.url}</div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
@@ -66,13 +91,13 @@ export async function getServerSideProps(context) {
         // assign the first key/value pair to bearer token cache
         const bearerToken = bearerTokenCache[0]
     // use request generated from above fetch to pass-in a bearer token to authenticate API call
-    const res = await fetch(`https://oauth.reddit.com/r/all/search/?q=${user.searchTerm}&limit=${user.threadLimit}&sort=${user.sortFilter}`, 
+    const res = await fetch(`https://oauth.reddit.com/r/all/search/?q=${searchTerm}&limit=${threadLimit}&sort=${sortFilter}`, 
         { headers: {
             // authorize with previously generated bearer_token here
             Authorization: `bearer ${bearerToken}`}
         })
     // take response generated from api call and return it as a json
-        // intialize return json to variable
+        // initialize return json to variable
     const rawData = await res.json()
     // then process rawData with a filter to remove it from its parent object
     const trimData = filter.isoChildObj(rawData)
@@ -85,4 +110,34 @@ export async function getServerSideProps(context) {
     }
 }
 
-export default page
+export default homePage
+
+
+
+// need to write userInput function accepts user search term
+// input: search term, thread limit, and sort category
+// output: initializes global variables with passed-in args 
+function userInput(search, limit, sort) {
+    try {
+        // if search and threadLimit are found and sort is missing
+        if (search && threadLimit > 25 || threadLimit < 100 && !sort) {
+            // initialize global variables to match user input
+            searchTerm = search
+            threadLimit = limit
+            console.log("Hot results are shown by default. Thread count caps at 100.") } 
+        // if search, threadLimit and sort are all found 
+        else if (search && threadLimit > 25 || threadLimit < 100 && sort.match(/['relevance','hot','new','comments', 'old']/)) {
+            // initialize all variables to passed-in args
+            console.log(`Sorting threads by ${sort}... Thread count caps at 100.`)
+            searchTerm = search
+            threadLimit = limit
+            sortFilter = sort } 
+        // if thread count is greater than 100 or sort doesn't match log error message
+        else if (threadLimit > 100 || !sort.match(['relevance','hot','new','comments', 'old'])) {
+            console.log("Bad search request, your results won't be accurate.") }
+    } 
+    // else return an error
+    catch (err){
+        console.log(err)
+    }    
+}
