839ff73c3263a060874703fbc3298d2959cb8457
Michael Isabella <98562265+izzy1090@users.noreply.github.com>
2022-12-15 17:33:12 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    USERNAME: 'mike-the-izz',
+    PASSWORD: 'Timeformore13!',
+    API_KEY: 'gFhtR4TLWPpZbZ5sFSZwXg',
+    API_SECRET: 'DP7IqOVj9_-Yhf1dqDNHSY3vKjFezA'
+  }
+}
+
+module.exports = nextConfig
