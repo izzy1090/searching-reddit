489d7b7451b74072e899313622e8b81ac015f829
izzy1090 <michaelisabella1@gmail.com>
2022-12-15 21:35:11 -0500

    Started work on a loading state

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,68 @@ import { useState } from 'react'
 import { DropDown, sortSelection } from './dropDownSortFilters';
 
 function Page ( {threads} ) {
+    const [isLoading, setLoading] = useState(false)
+
+    
+
+    const apiCall = async function getStaticProps (searchTerm) {
+        try {
+            setLoading(true)
+            console.log(isLoading)
+            console.log('searching...')
+            
+            // declared variables to use for OAuth with Reddit's API
+            const username = process.env.USERNAME;
+            const password = process.env.PASSWORD;
+            const api_key = process.env.API_KEY;
+            const api_secret = process.env.API_SECRET;
+            
+            const threads = await fetch('https://www.reddit.com/api/v1/access_token', {
+                method: 'POST',
+                headers: {
+                    // User-Agent was triggering a pre-flight response error, may enable at a later date
+                    // 'User-Agent': 'User agent',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    // use reddit app's api_key and api_secret for authorization
+                        // btoa is a method that creates a Base64-encoded ASCII string - basically converts binary to string
+                    'Authorization': 'Basic ' + Buffer.from(`${api_key}:${api_secret}`).toString('base64') },
+                    // use the associated account's username and password
+                    body: `grant_type=password&username=${username}&password=${password}`})
+                // await for response from reddit API and convert response into JSON
+                .then( returnedToken => returnedToken.json() )
+                // assign return data as key/value pairs to an empty object
+                .then( bearerTokenCache => Object.values(bearerTokenCache) )
+                // isolate the actual bearer token to return it for the API call below
+                .then( bearerToken => bearerToken[0] )
+            .then((returnedToken)=> {
+                return fetch(`https://oauth.reddit.com/r/all/search/?q=${searchTerm}&sort=${sortSelection}&limit=${100}`, 
+                    { headers: {
+                        // authorize with previously generated bearerToken here
+                        Authorization: `bearer ${returnedToken}`}
+                    })
+                // convert response from API call into a JSON
+                .then( response => response.json() )
+                // then take rawData and filter it through function components from other scripts
+                .then( rawData => {
+                    const childObject = rawData.data;
+                    setLoading(false)
+                    console.log(isLoading)
+                    return filterData(childObject);
+                })
+                
+            })
+            return {
+                props: {
+                    threads
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    
+
     // declare a state with a state variable userInput to initialize with user entered string
         // and a function to update our userInput state variable  
     const [userInput, setSearch] = useState('')
@@ -34,7 +96,7 @@ function Page ( {threads} ) {
                 // format returned date object into an appropriate string
                 const createdDate = `${responseDates.getMonth()}/${responseDates.getDate()}/${responseDates.getFullYear()}`
                 return (
-                    
+    
                     // Layout of how HTML tags for returned threads
                     <>
                     <div className="subreddits-container">
@@ -67,10 +129,16 @@ function Page ( {threads} ) {
                                                             target='_blank'>{results.permalink}</a>
                         </div>
                     </div>
+                    
                     </>
-                )
-            }))
-        })  
+                    
+                ) 
+            })) 
+       
+         }) 
+        // handleLoading(!isLoading)
+        // console.log(isLoading)
+        
     }
     // Intro page for when the user first loads the site 
         // Essentially if no threads are returned from the API, display the fragment below
@@ -141,51 +209,52 @@ function Page ( {threads} ) {
     )
 }
 
+
 export default Page 
 
-export const apiCall = async function getStaticProps (searchTerm) {
-    console.log('searching...')
-
-    // declared variables to use for OAuth with Reddit's API
-    const username = process.env.USERNAME;
-    const password = process.env.PASSWORD;
-    const api_key = process.env.API_KEY;
-    const api_secret = process.env.API_SECRET;
+// export const apiCall = async function getStaticProps (searchTerm) {
+//     console.log('searching...')
     
-    const threads = await fetch('https://www.reddit.com/api/v1/access_token', {
-        method: 'POST',
-        headers: {
-            // User-Agent was triggering a pre-flight response error, may enable at a later date
-            // 'User-Agent': 'User agent',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            // use reddit app's api_key and api_secret for authorization
-                // btoa is a method that creates a Base64-encoded ASCII string - basically converts binary to string
-            'Authorization': 'Basic ' + Buffer.from(`${api_key}:${api_secret}`).toString('base64') },
-            // use the associated account's username and password
-            body: `grant_type=password&username=${username}&password=${password}`})
-        // await for response from reddit API and convert response into JSON
-        .then( returnedToken => returnedToken.json() )
-        // assign return data as key/value pairs to an empty object
-        .then( bearerTokenCache => Object.values(bearerTokenCache) )
-        // isolate the actual bearer token to return it for the API call below
-        .then( bearerToken => bearerToken[0] )
-    .then((returnedToken)=> {
-        return fetch(`https://oauth.reddit.com/r/all/search/?q=${searchTerm}&sort=${sortSelection}&limit=${100}`, 
-            { headers: {
-                // authorize with previously generated bearerToken here
-                Authorization: `bearer ${returnedToken}`}
-            })
-        // convert response from API call into a JSON
-        .then( response => response.json() )
-        // then take rawData and filter it through function components from other scripts
-        .then( rawData => {
-            const childObject = rawData.data;
-            return filterData(childObject);
-        })
-    })
-    return {
-        props: {
-            threads
-        }
-    }
-}
+//     // declared variables to use for OAuth with Reddit's API
+//     const username = process.env.USERNAME;
+//     const password = process.env.PASSWORD;
+//     const api_key = process.env.API_KEY;
+//     const api_secret = process.env.API_SECRET;
+    
+//     const threads = await fetch('https://www.reddit.com/api/v1/access_token', {
+//         method: 'POST',
+//         headers: {
+//             // User-Agent was triggering a pre-flight response error, may enable at a later date
+//             // 'User-Agent': 'User agent',
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//             // use reddit app's api_key and api_secret for authorization
+//                 // btoa is a method that creates a Base64-encoded ASCII string - basically converts binary to string
+//             'Authorization': 'Basic ' + Buffer.from(`${api_key}:${api_secret}`).toString('base64') },
+//             // use the associated account's username and password
+//             body: `grant_type=password&username=${username}&password=${password}`})
+//         // await for response from reddit API and convert response into JSON
+//         .then( returnedToken => returnedToken.json() )
+//         // assign return data as key/value pairs to an empty object
+//         .then( bearerTokenCache => Object.values(bearerTokenCache) )
+//         // isolate the actual bearer token to return it for the API call below
+//         .then( bearerToken => bearerToken[0] )
+//     .then((returnedToken)=> {
+//         return fetch(`https://oauth.reddit.com/r/all/search/?q=${searchTerm}&sort=${sortSelection}&limit=${100}`, 
+//             { headers: {
+//                 // authorize with previously generated bearerToken here
+//                 Authorization: `bearer ${returnedToken}`}
+//             })
+//         // convert response from API call into a JSON
+//         .then( response => response.json() )
+//         // then take rawData and filter it through function components from other scripts
+//         .then( rawData => {
+//             const childObject = rawData.data;
+//             return filterData(childObject);
+//         })
+//     })
+//     return {
+//         props: {
+//             threads
+//         }
+//     }
+// }
