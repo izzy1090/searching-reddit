865481342ebe57d69fae2c0cd6809de6bb136d66
izzy1090 <michaelisabella1@gmail.com>
2023-03-15 15:24:30 -0400

    Squashed bugs causing the build to fail
    After adding conditional checks for both the media and data being
    received on the PostContent component as well as adding a check for
    content on the ThreadListPage, I was able to get the website to
    fully compile the build.
    
    Need to add a NSFW filter on any content that's flagged as NSFW.

--- pages/components/PostContent.js
+++ pages/components/PostContent.js
@@ -2,64 +2,66 @@ import { useState } from "react";
 
 function PostContent( {data, id, media} ){
     const [ expanded, setIsExpanded ] = useState({});
-    let content;
-    let renderedMedia;
-    const checkMediaFormat = media && media.includes('jpg') || 
-        media.includes('png')
-    if (checkMediaFormat){
-        renderedMedia = <>
-            <div className="flex items-center justify-center mt-2">
-                <a target="_blank" rel="noreferrer"
-                    href={media}>
-                    <img src={media} alt='Embedded images from Reddit post.'/>
-                </a>
-            </div>
-        </>
-    }
-    // Function passes the posts ID to the setter function
-    const handleExpand = (postId) => {
-        // if the postId in the expanded state is false
-        if (!expanded[postId]){
-            // then spread prevExpanded states and set the current one to be true - handles show button
-            setIsExpanded(prevExpanded => ({...prevExpanded, [postId]: true}))
-        // otherwise spread the previous states and set them to false - handles hide button
-        } else setIsExpanded(prevExpanded => ({...prevExpanded, [postId]: false}))
-    }
-    // then init. the expanded variable to the expanded state with the post's ID as a key
-    const isExpanded = expanded[id];
+    if (data || media){
+        let content;
+        let renderedMedia;
+        const checkMediaFormat = media && (media.includes('jpg') || 
+            media.includes('png'))
+        if (checkMediaFormat){
+            renderedMedia = <>
+                <div className="flex items-center justify-center mt-2">
+                    <a target="_blank" rel="noreferrer"
+                        href={media}>
+                        <img src={media} alt='Embedded images from Reddit post.'/>
+                    </a>
+                </div>
+            </>
+        }
+        // Function passes the posts ID to the setter function
+        const handleExpand = (postId) => {
+            // if the postId in the expanded state is false
+            if (!expanded[postId]){
+                // then spread prevExpanded states and set the current one to be true - handles show button
+                setIsExpanded(prevExpanded => ({...prevExpanded, [postId]: true}))
+            // otherwise spread the previous states and set them to false - handles hide button
+            } else setIsExpanded(prevExpanded => ({...prevExpanded, [postId]: false}))
+        }
+        // then init. the expanded variable to the expanded state with the post's ID as a key
+        const isExpanded = expanded[id];
 
-    if (data.length > 500){
-        // if isExpanded has a value
-        if (isExpanded){
-            // we want to initialize any posts with the true isExpanded variable to display
-            return (content = <div className="p-1 text-sm">
-                <div className="overflow-auto whitespace-pre-wrap ">
+        if (data.length > 500){
+            // if isExpanded has a value
+            if (isExpanded){
+                // we want to initialize any posts with the true isExpanded variable to display
+                return (content = <div className="p-1 text-sm">
+                    <div className="overflow-auto whitespace-pre-wrap ">
+                        {data}   
+                    </div>
+                    {renderedMedia}
+                    <button onClick={()=>handleExpand(id)} 
+                        className="mt-1 text-slate-500 text-xs font-semibold hover:underline">
+                        Hide
+                    </button>
+                </div>)
+            } else return content = <div className="p-1 text-sm">
+                <div className="truncate overflow-auto max-h-40 whitespace-pre-wrap">
                     {data}   
                 </div>
                 {renderedMedia}
                 <button onClick={()=>handleExpand(id)} 
-                    className="mt-1 text-slate-500 text-xs font-semibold hover:underline">
-                    Hide
+                    className="mt-2 pt-1 text-slate-500 text-xs font-semibold hover:underline">
+                    See more...
                 </button>
-            </div>)
-        } else return content = <div className="p-1 text-sm">
-            <div className="truncate overflow-auto max-h-40 whitespace-pre-wrap">
+            </div>
+        // otherwise render post normally
+        } else content = <div className="p-1 text-sm">
+            <div className="overflow-auto whitespace-pre-wrap">
                 {data}   
             </div>
             {renderedMedia}
-            <button onClick={()=>handleExpand(id)} 
-                className="mt-2 pt-1 text-slate-500 text-xs font-semibold hover:underline">
-                See more...
-            </button>
         </div>
-    // otherwise render post normally
-    } else content = <div className="p-1 text-sm">
-        <div className="overflow-auto whitespace-pre-wrap">
-            {data}   
-        </div>
-        {renderedMedia}
-    </div>
-    return content;
+        return content;
+    }
 };
 
 export default PostContent;
